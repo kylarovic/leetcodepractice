10f78f6b31341ff6c2a6a8c1d81fffc93f37d7bb
Kyle <kylarovic@gmail.com>
2016-05-30 20:25:32 -0700

    Envelopes - Fix alogrithm. Greedy didn't work, switched to DP

--- src/main/java/Envelopes.java
+++ src/main/java/Envelopes.java
@@ -1,4 +1,5 @@
-import com.leetcode.Utils.Utility;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.*;
 
@@ -6,8 +7,8 @@ import java.util.*;
  * Created on 5/26/16.
  */
 public class Envelopes {
+
     public int envelopes(int[][] env) {
-        int result = 0;
         Arrays.sort(env, new Comparator<int[]>() {
             public int compare(int[] o1, int[] o2) {
                 if (o1[0] < o2[0])
@@ -22,52 +23,50 @@ public class Envelopes {
             }
         });
 
-        List<Integer> widths = new ArrayList<Integer>();
-        List<Integer> res = new ArrayList<Integer>();
-        widths.add(env[0][1]);
-        for(int i = 1; i < env.length; i++) {
-            if(env[i-1][0] != env[i][0]) {
-                widths.add(env[i][1]);
+        int[] dp = new int[env.length];
+        int result = 0;
+        Arrays.fill(dp, 1);
+        for(int i = 0; i < env.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(env[i][1] > env[j][1] && env[i][0] > env[j][0]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+                result = Math.max(dp[i], result);
             }
         }
-
-        for(int i = 0; i < widths.size(); i++) {
-            int idx = binarySearch(res, widths.get(i));
-            if(idx < res.size()) {
-                res.set(idx, widths.get(i));
-            }else {
-                res.add(widths.get(i));
-            }
-            System.out.println(res);
-        }
-        //System.out.println(widths);
-        //Utility.printArray(env);
         return result;
     }
+    @Test
+    public void test() {
+        int[][] env = new int[][] {
+                {2,1},
+                {3,2},
+                {4,3},
+                {5,3},
+                {5,4},
+                {5,5},
+                {6,5},
+                {7,6},
+                {8,7},
+                {9,1}
+        };
+        Assert.assertEquals(7, envelopes(env));
 
-    public int binarySearch(List<Integer> result, int key) {
-        int low = 0, high = result.size()-1;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(result.get(mid) == key) {
-                if(mid == 0) return mid; // last element so return
-                if(result.get(mid-1) == key) {
-                    high = mid - 1; // moving left
-                }else {
-                    return mid;
-                }
-            }else if (result.get(mid) < key) {
-                low = mid + 1;
-            }else {
-                high = mid - 1;
-            }
-        }
-        if(low == result.size()-1 && result.get(low) < key) {
-            return low + 1; // if key not found, and low points to last element.
-        }
-        return low;
+        env = new int[][] {
+                {2,100},
+                {3,2},
+                {4,3},
+                {5,3},
+                {5,4},
+                {5,5}
+        };
+        Assert.assertEquals(3, envelopes(env));
+
+        env = new int[][] {
+                {2,100},{3,200},{4,300},{5,250},{5,400},{5,500},{6,350},{6,360},{7,380}
+        };
+        Assert.assertEquals(5, envelopes(env));
     }
-
     public static void main(String[] args) {
         Envelopes e = new Envelopes();
         int[][] env = new int[][] {
